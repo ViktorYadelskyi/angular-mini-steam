0ecc1bc986e519948b3c4aa49209402435194c80
Viktor <yadelskyiviktor@gmail.com>
2022-02-22 20:47:43 +0200

    deploy fix

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<angular-mini-steam>'));
+app.use(express.static(`./dist/angular-mini-steam`));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<angular-mini-steam>/index.html'));});
+'/dist/angular-mini-steam/index.html'));});
 app.listen(process.env.PORT || 8080);
